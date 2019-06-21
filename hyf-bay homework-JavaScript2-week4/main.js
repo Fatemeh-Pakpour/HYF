1c1f06fc3ae0dd2ff580d76f6ba75af39eaa71fb
Olha Afanasieva <afanasievaolja@gmail.com>
2019-06-21 09:38:53 +0200

    hyf-bay JS2-week4 - render lists of products

--- hyf-bay homework-JavaScript2-week4/main.js
+++ hyf-bay homework-JavaScript2-week4/main.js
@@ -1,3 +1,51 @@
 console.log('Script loaded');
 
-console.log(getAvailableProducts());
+//Render test product names
+function renderProducts(arr){
+    let productsList = document.querySelector('section.products ul');
+
+    arr.forEach(x => {
+        let productsListli = document.createElement('li');
+        let productsElement = document.createElement('ul');
+
+        for(let key in x){
+
+            let existArray = x[key];
+            let productsElementli = document.createElement('li');
+
+            if(key === 'id'){
+                productsElementli.innerHTML = `${x[key]}`;
+            }
+            else if(Array.isArray(existArray)){
+                let arrayList = document.createElement('ul');
+
+                let arrayListFirstLi = document.createElement('li');
+
+                arrayListFirstLi.innerHTML = `${key}: `;
+                arrayList.appendChild(arrayListFirstLi);
+
+                existArray.forEach(y => {
+
+                    let arrayListLi = document.createElement('li');
+                    
+                    arrayListLi.innerHTML = y;
+                    arrayList.appendChild(arrayListLi);
+                })
+               
+                productsElementli.appendChild(arrayList);
+            }
+            else{
+                productsElementli.innerHTML = `${key} : ${x[key]}`;  
+            }
+
+            productsElement.appendChild(productsElementli);
+        }
+
+        productsListli.appendChild(productsElement);
+        productsList.appendChild(productsListli);
+    })
+}
+
+const products = getAvailableProducts();
+
+renderProducts(products);
