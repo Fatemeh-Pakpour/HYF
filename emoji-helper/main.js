57242e76c72e4a1b9fe5160a1c3f977595a90019
Olha Afanasieva <afanasievaolja@gmail.com>
2019-08-06 22:23:19 +0200

    emoji-helper Active category. Search works with category.

--- emoji-helper/main.js
+++ emoji-helper/main.js
@@ -1,120 +1,145 @@
-const emojiiApi = 'https://raw.githubusercontent.com/amio/emoji.json/master/emoji.json';
-let listOfEmoji;
-const renderEmoji = document.getElementById('renderEmoji');
-const categoriesEmoji = document.getElementById('categoriesEmoji');
-const searchString = document.getElementById('searchString');
-const textArea = document.getElementById('textArea');
-const favouriteEmojis = document.getElementById('favouriteEmojis');
-let listOfFavouriteEmoji = JSON.parse(localStorage.getItem('emoji')) || [];
+(() => {
+    const emojiiApi = 'https://raw.githubusercontent.com/amio/emoji.json/master/emoji.json';
+    const renderedEmoji = document.getElementById('renderedEmoji');
+    const categoriesEmoji = document.getElementById('categoriesEmoji');
+    const searchString = document.getElementById('searchString');
+    const textArea = document.getElementById('textArea');
+    const favouriteEmojis = document.getElementById('favouriteEmojis');
+    let listOfFavouriteEmoji = JSON.parse(localStorage.getItem('emoji')) || [];
+    let currentCategory = '';
 
-function fetchEmojiiList(api) {
-    fetch(api)
-        .then(response => response.json())
-        .then(data => {
-            listOfEmoji = data;
-            renderListOfEmoji(listOfEmoji);
-            renderCategories(listOfEmoji);
-        })
-}
-
-fetchEmojiiList(emojiiApi);
-
-searchString.addEventListener('keyup', () => {
-    const valueForSearch = searchString.value;
-    let searchList = listOfEmoji.filter(element => {
-        return element.name.toLowerCase().includes(valueForSearch.toLowerCase());
-    })
-    renderListOfEmoji(searchList);
-});
-
-function renderFavouriteEmojii(arr){
-    favouriteEmojis.innerHTML = '';
-    arr.forEach(favouriteEmoji => {
-        const itemFavouriteEmoji = document.createElement('li');
-        itemFavouriteEmoji.innerHTML = favouriteEmoji;
-        favouriteEmojis.appendChild(itemFavouriteEmoji);
-
-        itemFavouriteEmoji.addEventListener('click', () => {
-            textArea.value += favouriteEmoji;
-            writeToClipboard(favouriteEmoji)
-        })
-    })
-}
-
-function renderCategories(arr){
-    const categories = Array.from(new Set(arr.map(emoji => {
-        return emoji.category.split('(')[0];
-    })))
-
-    const liAllCategories = document.createElement('li');
-    liAllCategories.innerHTML = 'All';
-    categoriesEmoji.appendChild(liAllCategories);
-
-    categories.forEach(category => {
-        
-        const liCategory = document.createElement('li');
-        liCategory.innerHTML = category;
-        categoriesEmoji.appendChild(liCategory);
-
-        liAllCategories.addEventListener('click', () => {
-            renderListOfEmoji(arr);
-        });
-
-        liCategory.addEventListener('click', () => {
-            let filterByCategoryArray = arr.filter(emoji => {
-                return emoji.category.includes(category);
+    function fetchEmojiiList(api) {
+        fetch(api)
+            .then(response => response.json())
+            .then(listOfEmoji => {
+                renderListOfEmoji(listOfEmoji);
+                renderCategories(listOfEmoji);
+                searchString.addEventListener('keyup', () => {
+                    renderListOfEmoji(listOfEmoji);
+                });
             })
-            renderListOfEmoji(filterByCategoryArray);
-        });
-    })
-}
+    }
 
-function renderListOfEmoji(arr) {
-    renderEmoji.innerHTML = ''; 
-    favouriteEmojis.innerHTML = '';
-    renderFavouriteEmojii(listOfFavouriteEmoji);
-    arr.forEach(emoji => {
+    fetchEmojiiList(emojiiApi);
+
+    function renderFavouriteEmojii(arr) {
+        favouriteEmojis.innerHTML = '';
+        arr.forEach(favouriteEmoji => {
+            const liFavouriteEmoji = document.createElement('li');
+            liFavouriteEmoji.innerHTML = favouriteEmoji;
+            favouriteEmojis.appendChild(liFavouriteEmoji);
+
+            liFavouriteEmoji.addEventListener('click', () => {
+                addEmojiToTheString(favouriteEmoji);
+            })
+        })
+    }
+
+    function renderCategories(arr) {
+        const categories = Array.from(new Set(arr.map(emoji => {
+            return emoji.category.split('(')[0];
+        })));
+
+        const domCategories = [];
+
+        function resetClass(){
+            domCategories.forEach(domElement => {
+                if(currentCategory === domElement.dataset.category){
+                    domElement.setAttribute('class', 'categories-active');
+                }
+                else{
+                    domElement.classList.remove('categories-active');                    
+                }
+            });            
+        }
         
-        const li = document.createElement('li');
-        const spanImage = document.createElement('span');
-        const spanName = document.createElement('span');
-        const spanFavourite = document.createElement('span');
-        spanImage.setAttribute('class', 'span-image');
-        spanName.setAttribute('class', 'span-name');
-        spanFavourite.setAttribute('class', 'favourite');
-        spanImage.innerHTML = emoji.char;
-        spanName.innerHTML = emoji.name;
-        spanFavourite.innerHTML = `&#9733`;
-        li.appendChild(spanImage);
-        li.appendChild(spanName);
-        li.appendChild(spanFavourite);
+        ['', ...categories].forEach(category => {
+            const liCategory = document.createElement('li');
+            liCategory.innerHTML = category || 'All';
+            liCategory.dataset.category = category;
+            categoriesEmoji.appendChild(liCategory);
+            domCategories.push(liCategory);
 
-        spanFavourite.addEventListener('click', (e) => {   
-            e.stopPropagation();
-            listOfFavouriteEmoji.push(emoji.char);
-            listOfFavouriteEmoji = Array.from(new Set(listOfFavouriteEmoji));
-            if(listOfFavouriteEmoji.length > 20){
-                listOfFavouriteEmoji.splice(0, 1);
+            liCategory.addEventListener('click', () => {                
+                currentCategory = category;
+                resetClass();  
+                renderListOfEmoji(arr);
+            });
+        });
+        resetClass();        
+    }
+
+    function addEmojiToTheString(emojiImage) {
+        textArea.value += emojiImage;
+        writeToClipboard(emojiImage);
+    }
+
+    function renderListOfEmoji(arr) {
+        renderedEmoji.innerHTML = '';
+        favouriteEmojis.innerHTML = '';
+
+        const valueForSearch = searchString.value;
+        let searchList = arr.filter(emoji => {
+            if (valueForSearch !== ''
+                && !emoji.name.toLowerCase().includes(valueForSearch.toLowerCase())) {
+                return false;
             }
-
-            let arrayToStore = JSON.stringify(listOfFavouriteEmoji);
-            localStorage.setItem('emoji', arrayToStore);
-
-            renderFavouriteEmojii(listOfFavouriteEmoji);
+            if (!emoji.category.includes(currentCategory)) {
+                return false;
+            }
+            return true;
         });
 
-        li.addEventListener('click', () => {
-            textArea.value += emoji.char;
-            writeToClipboard(emoji.char)
+        renderFavouriteEmojii(listOfFavouriteEmoji);
+
+        let innerArray = searchList || arr;
+        innerArray.forEach(emoji => {
+
+            const li = document.createElement('li');
+            const spanImage = document.createElement('span');
+            const spanName = document.createElement('span');
+            const spanFavourite = document.createElement('span');
+
+            spanImage.setAttribute('class', 'span-image');
+            spanName.setAttribute('class', 'span-name');
+            spanFavourite.setAttribute('class', 'favourite');
+
+            spanImage.innerHTML = emoji.char;
+            spanName.innerHTML = emoji.name;
+            spanFavourite.innerHTML = `&#9733`;
+
+            li.appendChild(spanImage);
+            li.appendChild(spanName);
+            li.appendChild(spanFavourite);
+
+            spanFavourite.addEventListener('click', (event) => {
+                event.stopPropagation();                
+                
+                if(!listOfFavouriteEmoji.includes(emoji.char)){
+                    listOfFavouriteEmoji.splice(0, 0, emoji.char);
+                }
+                if (listOfFavouriteEmoji.length > 20) {
+                    listOfFavouriteEmoji.pop();
+                }
+                let arrayToStore = JSON.stringify(listOfFavouriteEmoji);
+                localStorage.setItem('emoji', arrayToStore);
+
+                renderFavouriteEmojii(listOfFavouriteEmoji);
+            });
+
+            li.addEventListener('click', () => {
+                addEmojiToTheString(emoji.char);
+            })
+
+            renderedEmoji.appendChild(li);
         });
+    }
 
-        renderEmoji.appendChild(li);
-    });
-}
-
+})();
 
 
 
-
-
+//Work later on these tasks
+//li.innerHTML = `<span></span><span></span><span></span>`
+//display:none to li element instead rerender every time
 
